7b234d7eb841d47a6f704a7fb3cda3850d70a0f7
Tom Kwong <tk3369@gmail.com>
2013-07-20 10:54:52 -0700

    added exported function log-set-logger! refactoring code to use shorter variable names

--- logger.scm
+++ logger.scm
@@ -12,6 +12,7 @@
 (module logger 
 	(log-debug log-info log-warning log-error
 	 log-get-verbosity log-set-verbosity!
+	 log-set-logger! 
 	 log-set-output-file!
 	 log-reset-output-port!)
 	
@@ -19,54 +20,61 @@
 (use srfi-1)
 (use posix)
 
-;; default values
-(define log-default-verbosity-level 2) ; 0=error 1=warning 2=info 3=debug
-(define log-default-output-port ##sys#standard-output)
-
-;; global variables
-(define log-verbosity-level log-default-verbosity-level)
-(define log-output-port log-default-output-port)
-
 ;; this private function requires posix
 (define (%current-datetime-string)
   (time->string (seconds->local-time (current-seconds)) "%Y-%m-%d %H:%M:%S"))
 
 ;; private function that logs data into the output port
-;; TODO need to make this function thread safe?
-;; TODO add buffer size to determine when to flush?
-(define (%log-function . args)
-  (for-each (lambda (x) (display x log-output-port))
-	    (append (list (%current-datetime-string) " ") args '(#\newline)))
-  (flush-output log-output-port)
+(define (%default-logger . args)
+  (for-each (lambda (x) 
+	      (display x output-port)
+	      (display " " output-port))
+	    (append 
+	     (list (%current-datetime-string))
+	     args))
+  (display #\newline output-port)
+  (flush-output output-port)
   #t)
 
+;; default values
+(define default-verbosity-level 2) ; 0=error 1=warning 2=info 3=debug
+(define default-output-port ##sys#standard-output)
+
+;; global variables
+(define verbosity-level default-verbosity-level)
+(define output-port default-output-port)
+(define logger %default-logger)
+
 (define (log-debug . args) 
-  (if (> log-verbosity-level 2)
-      (apply %log-function (cons "DEBUG: " args))
+  (if (> verbosity-level 2)
+      (apply logger (cons "DEBUG  " args))
       #f))
 
 (define (log-info . args) 
-  (if (> log-verbosity-level 1)
-      (apply %log-function (cons "INFO: " args))
+  (if (> verbosity-level 1)
+      (apply logger (cons "INFO   " args))
       #f))
 
 (define (log-warning . args)
-  (if (> log-verbosity-level 0)
-      (apply %log-function (cons "WARNING: " args))
+  (if (> verbosity-level 0)
+      (apply logger (cons "WARNING" args))
       #f))
 
 (define (log-error . args) 
-  (apply %log-function (cons "ERROR: " args)))
+  (apply logger (cons "ERROR  " args)))
 
 (define (log-get-verbosity)
-  log-verbosity-level)
+  verbosity-level)
 
 (define (log-set-verbosity! v)
   (let* ((sym '(error warning info debug))
 	 (idx (list-index eq? sym (circular-list v))))
     (if idx
-	(set! log-verbosity-level idx)
+	(set! verbosity-level idx)
 	(log-error "invalid verbosity label: " v "; it must be one of '(" sym ")"))))
+
+(define (log-set-logger! f)
+  (set! logger f))
 
 (define (log-set-output-file! filepath)
   (call-with-current-continuation
@@ -77,14 +85,14 @@
         (k e))
       (lambda ()
 	;; close previous port 
-	(if (not (eq? log-output-port log-default-output-port))
-	    (close-output-port log-output-port))
+	(if (not (eq? output-port default-output-port))
+	    (close-output-port output-port))
 	;; open new output port
 	(let ((p (open-output-file filepath #:append)))
 	  (print "logger opened output file " filepath)
-	  (set! log-output-port p))))))) 
+	  (set! output-port p))))))) 
 
 (define (log-reset-output-port!)
-  (set! log-output-port log-default-output-port))
+  (set! output-port default-output-port))
 
 ) ; end module
